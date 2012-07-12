2ffe89a21f65f51245b0df6d5ece67d678b59092
Joe Cheng <joe@rstudio.org>
2012-07-12 14:00:12 -0700

    work in progress

--- R/timer.R
+++ R/timer.R
@@ -3,3 +3,51 @@
 now <- function() {
   .Call('getTimeInMillis')
 }
+
+TimerCallbacks <- setRefClass(
+  'TimerCallbacks',
+  fields = list(
+    .funcs = 'Map',
+    .times = 'data.frame'
+  ),
+  methods = list(
+    schedule = function(millis, func) {
+      # TODO: Make a unique ID
+      id <- 'a0'
+      t <- now()
+      
+      # TODO: Horribly inefficient, use a heap instead
+      .times <<- rbind(.times, data.frame(time=t+millis,
+                                          scheduled=t,
+                                          id=id))
+      .times <<- .times[order(.times$time),]
+      
+      .funcs$set(id, func)
+      
+      return(id)
+    },
+    timeToNextEvent = function() {
+      if (length(.times) == 0)
+        return(Inf)
+      return(.times[1, 'time'] - now())
+    },
+    takeElapsed = function() {
+      t <- now()
+      elapsed <- .times$time < now()
+      result <- .times[elapsed,]
+      .times <<- .times[!elapsed,]
+      
+      # TODO: Examine scheduled column to check if any funny business
+      #       has occurred with the system clock (e.g. if scheduled
+      #       is later than now())
+      
+      return(result)
+    },
+    executeElapsed = function() {
+      elapsed <- takeElapsed()
+      
+    }
+  )
+)
+
+timerCallbacks <- TimerCallbacks$new()
