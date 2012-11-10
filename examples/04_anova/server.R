299d100e6edac6b174bd9fca0343b3b50d1d1739
Marcio Nicolau <marcionicolau@me.com>
2012-11-10 11:47:08 -0200

    Anova Example

---
+++ examples/04_anova/server.R
@@ -0,0 +1,7 @@
+library(shiny)
+
+# Define server logic require to analysis
+
+shinyServer(function(input, output) {
+
+})
---
+++ examples/04_anova/ui.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+#Define UI for anova
+shinyUI(pageWithSidebar(
+
+  # Application title
+  headerPanel("Anova"),
+
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+    selectInput("variable", "Variable:",
+                list("Cylinders" = "cyl", 
+                     "Transmission" = "am", 
+                     "Gears" = "gear")),
+    
+    checkboxInput("outliers", "Show outliers", FALSE)
+  ),
+
+  mainPanel()
+
+))
